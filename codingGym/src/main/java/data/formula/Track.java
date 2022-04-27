72d9ed9e62c8b66d237514b0df66dd8cdded0404
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-04-28 00:00:18 +0300

    Add files via upload

---
+++ codingGym/src/main/java/data/formula/Track.java
@@ -0,0 +1,4 @@
+package data.formula;
+
+public class Track {
+}
---
+++ codingGym/src/main/java/workspace/Lesson.java
@@ -0,0 +1,9 @@
+package workspace;
+
+public class Lesson {
+    public static void main(String[] args) {
+        var nums = new int[]{1, 2, 11, 28, 35, 90, 111, 54, 3, 17, 42, 53, 100};
+        var names = "Ann, John, Billy, Viktor, Shrek, Bob";
+        var str1 = new String[]{"lake", "bob", "cat", "madam", "level"};
+    }
+}
